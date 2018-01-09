e8000717d3967013d25d3efd054306d6f1328cb4
Theresa Ma <theresa01.ma@gmail.com>
2018-01-08 19:13:41 -0800

    remove book scripts and update book yaml

--- books/getgoodreads.sh
+++
@@ -1,5 +0,0 @@
-source ../../venv/bin/activate
-wget -O ../_data/currently-reading.xml https://www.goodreads.com/review/list/8086458.xml?key=AyBL1dP9FjhaN6spswSxg&v=2&user_id=8086458-theresa&shelf=currently-reading&per_page=200
-wget -O ../_data/read.xml https://www.goodreads.com/review/list/8086458.xml?key=AyBL1dP9FjhaN6spswSxg&v=2&user_id=8086458-theresa&shelf=read&per_page=200
-python xml2yaml.py
-deactivate
